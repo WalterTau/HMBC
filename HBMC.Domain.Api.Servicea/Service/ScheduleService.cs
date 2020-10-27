2689670ef5ec3c94370d6743ec3e2e533f2a5bdc
Walter Tau <waltert@eohmc.onmicrosoft.com>
2020-10-27 08:44:41 +0200

    Add getall and byId schedule service

--- HBMC.Domain.Api.Servicea/Service/ScheduleService.cs
+++ HBMC.Domain.Api.Servicea/Service/ScheduleService.cs
@@ -1,7 +1,10 @@
 ﻿using HBMC.Domain.Api.Models;
 using HBMC.Domain.Api.Services.Interface;
+using HBMC.Domain.Api.Services.SharePointOnlineServiceHelper;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +12,8 @@ namespace HBMC.Domain.Api.Services.Service
 {
     public class ScheduleService : IScheduleService
     {
+        private IConfiguration _configuration;
+        private ISharePointServiceHelper _sharePointServiceHelper;
         public async Task<Schedule> Add(Schedule model)
         {
             throw new NotImplementedException();
@@ -21,12 +26,14 @@ namespace HBMC.Domain.Api.Services.Service
 
         public async Task<IEnumerable<Schedule>> GetAllSchedules()
         {
-            throw new NotImplementedException();
+            var model = await _sharePointServiceHelper.GetScheduleSharePointList();
+            return model;
         }
 
         public async Task<Schedule> GetById(string id)
         {
-            throw new NotImplementedException();
+            var model = await _sharePointServiceHelper.GetScheduleSharePointList();
+            return model.Where(i => i.Id == id).FirstOrDefault();
         }
 
         public async Task<Schedule> Update()
