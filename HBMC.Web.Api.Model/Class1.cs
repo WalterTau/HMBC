04dd56fc2c05d248497c75607dddcb29a0c31bbf
Walter Tau <waltert@eohmc.onmicrosoft.com>
2020-10-28 13:03:42 +0200

    Refactor the Project Structure by cleaning deleted Projects

--- HBMC.Web.Api.Model/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HBMC.Web.Api.Model
-{
-    public class Class1
-    {
-    }
-}
--- HBMC.Web.Api.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HBMC.Web.Api.Services
-{
-    public class Class1
-    {
-    }
-}
